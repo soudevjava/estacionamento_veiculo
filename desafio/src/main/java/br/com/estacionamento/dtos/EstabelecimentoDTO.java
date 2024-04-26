a844272683382bee9e0e39aad21aa6467ca26b9f
Renato28 <renatonobrega87@gmail.com>
2024-04-25 23:28:32 -0300

    implementando mensagens de validações em alguns campos dos dtos

--- desafio/src/main/java/br/com/estacionamento/dtos/EstabelecimentoDTO.java
+++ desafio/src/main/java/br/com/estacionamento/dtos/EstabelecimentoDTO.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,19 +19,28 @@ import org.hibernate.validator.constraints.br.CNPJ;
 public class EstabelecimentoDTO {
 
     private Long id;
-    @NotNull(message = "Este campo é de preenchimento obrigatório")
+
+    @NotNull(message = "O Nome do Estabelecimento é de preenchimento obrigatório")
+    @Size(min = 3, max = 50, message = "O Nome do Estabelecimento deve ter entre 3 e 50 caracteres")
     private String nome;
+
     @Pattern(regexp = "^\\d{2}\\.\\d{3}\\.\\d{3}/\\d{4}-\\d{2}$")
-    @NotNull(message = "Este campo é de preenchimento obrigatório")
+    @Size(min = 14, max = 14, message = "O CNPJ do Estabelecimento deve ter 14 caracteres")
+    @NotNull(message = "O CNPJ do Estabelecimento é de preenchimento obrigatório")
     private String cnpj;
-    @NotNull(message = "Este campo é de preenchimento obrigatório")
+
+    @NotNull(message = "O Endereço do Estabelecimento é de preenchimento obrigatório")
     private String endereco;
+
     @Pattern(regexp= "^\\(\\d{2}\\)\\s\\d{4}-\\d{4}$")
-    @NotNull(message = "Este campo é de preenchimento obrigatório")
+    @NotNull(message = "O Telefone do Estabelecimento é de preenchimento obrigatório")
+    @Size(min = 10, max = 11, message = "O Telefone do Estabelecimento deve ter entre 10 e 11 caracteres")
     private String telefone;
-    @NotNull(message = "Este campo é de preenchimento obrigatório")
+
+    @NotNull(message = "A Quantidade de Vagas para Motos do Estabelecimento é de preenchimento obrigatório")
     private Integer quantidadeVagasMotos;
-    @NotNull(message = "Este campo é de preenchimento obrigatório")
+
+    @NotNull(message = "A Quantidade de Vagas para Carros do Estabelecimento é de preenchimento obrigatório")
     private Integer quantidadeVagasCarros;
 
     public EstabelecimentoDTO(String message) {
--- desafio/src/main/java/br/com/estacionamento/dtos/VeiculoDTO.java
+++ desafio/src/main/java/br/com/estacionamento/dtos/VeiculoDTO.java
@@ -1,12 +1,12 @@
 package br.com.estacionamento.dtos;
 
-import br.com.estacionamento.model.Estabelecimento;
 import br.com.estacionamento.model.Tipo;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,19 +19,29 @@ import lombok.Setter;
 public class VeiculoDTO {
 
     private Long id;
-    @NotBlank(message = "Este campo é de preenchimento obrigatório")
+
+    @NotBlank(message = "A Marca do Veiculo é de preenchimento obrigatório")
+    @Size(min = 3, max = 20, message = "A Marca do Veiculo deve ter entre 3 e 20 caracteres")
     private String marca;
-    @NotBlank(message = "Este campo é de preenchimento obrigatório")
+
+    @NotBlank(message = "O Modelo do Veiculo é de preenchimento obrigatório")
+    @Size(min = 3, max = 30, message = "O Modelo do Veiculo deve ter entre 3 e 30 caracteres")
     private String modelo;
-    @NotBlank(message = "Este campo é de preenchimento obrigatório")
+
+    @NotBlank(message = "A Cor do Veiculo é de preenchimento obrigatório")
+    @Size(min = 3, max = 10, message = "A Cor do Veiculo deve ter entre 3 e 10 caracteres")
     private String cor;
+
     @Pattern(regexp = "^[A-Z]{3}-?[0-9]{4}$")
-    @NotBlank(message = "Este campo é de preenchimento obrigatório")
+    @NotBlank(message = "A Placa do Veiculo é de preenchimento obrigatório")
+    @Size(min = 7, max = 7, message = "A Placa do Veiculo deve ter 7 caracteres")
     private String placa;
-    @NotNull(message = "Este campo é de preenchimento obrigatório")
+
+    @NotNull(message = "O Tipo do Veiculo é de preenchimento obrigatório")
     @Enumerated(EnumType.STRING)
     private Tipo tipo;
-    @NotNull(message = "Este campo é de preenchimento obrigatório")
+
+    @NotNull(message = "O Id do Estabelecimento do Veiculo é de preenchimento obrigatório")
     private Long idEstabelecimento;
 
     public VeiculoDTO(String message) {
