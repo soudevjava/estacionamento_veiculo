f574a9c1838057e2f546c071e9716deb486416f5
Renato28 <renatonobrega87@gmail.com>
2024-05-09 00:04:39 -0300

    corrigindo nome do metodo no repository de estabelecimento

--- desafio/src/main/java/br/com/estacionamento/repository/EstabelecimentoRepository.java
+++ desafio/src/main/java/br/com/estacionamento/repository/EstabelecimentoRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 @Repository
 public interface EstabelecimentoRepository extends JpaRepository<Estabelecimento, Long> {
 
-    Optional<Estabelecimento> findByName(String nome);
+    Optional<Estabelecimento> findByNome(String nome);
 }
