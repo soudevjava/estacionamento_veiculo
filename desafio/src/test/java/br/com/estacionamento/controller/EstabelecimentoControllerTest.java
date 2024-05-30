31aec844c49f45efb379d25ca37a04604b1b25f1
leonardograut <omgleo@live.com>
2024-05-29 22:51:26 -0300

    Testando  Metodo EstabelecimentoController findall,

--- desafio/src/test/java/br/com/estacionamento/controller/EstabelecimentoControllerTest.java
+++ desafio/src/test/java/br/com/estacionamento/controller/EstabelecimentoControllerTest.java
@@ -4,6 +4,7 @@ import br.com.estacionamento.builder.EstabelecimentoRequestDTOBuilder;
 import br.com.estacionamento.builder.EstabelecimentoResponseDTOBuilder;
 import br.com.estacionamento.dtos.request.EstabelecimentoRequestDTO;
 import br.com.estacionamento.dtos.response.EstabelecimentoResponseDTO;
+import br.com.estacionamento.dtos.response.RegistroEstacionamentoResponseDTO;
 import br.com.estacionamento.service.EstabelecimentoService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,19 +14,33 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
+import java.util.Collections;
+import java.util.List;
+
 import static br.com.estacionamento.utils.JsonConvertionUtils.asJsonString;
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.web.servlet.function.RequestPredicates.param;
 
 @ExtendWith(MockitoExtension.class)
 public class EstabelecimentoControllerTest {
@@ -39,6 +54,8 @@ public class EstabelecimentoControllerTest {
 
     @InjectMocks
     private EstabelecimentoController estabelecimentoController;
+
+
 
     @BeforeEach
     void setUp() {
@@ -56,7 +73,7 @@ public class EstabelecimentoControllerTest {
         EstabelecimentoResponseDTO responseDTO = EstabelecimentoResponseDTOBuilder.builder().build().toEstabelecimentoResponseDTO();
 
 
-        when(estabelecimentoService.cadastrar(requestDTO)).thenReturn(responseDTO);
+        when(estabelecimentoController.cadastrar(requestDTO)).thenReturn(responseDTO);
 
         mockMvc.perform(post(ESTABELECIMENTO_API_URL_PATH)
                         .contentType(MediaType.APPLICATION_JSON)
@@ -77,4 +94,59 @@ public class EstabelecimentoControllerTest {
                 .andExpect(jsonPath("$.quantidadeVagasCarros", is(responseDTO.getQuantidadeVagasCarros())));
 
     }
+
+    @Test
+    void WhenFindAllThenReturnAnListOfUsers() throws Exception {
+
+        Pageable pageable = PageRequest.of(0,10);
+        assertNotNull(pageable);
+
+        EstabelecimentoResponseDTO responseDTO = EstabelecimentoResponseDTOBuilder.builder().build().toEstabelecimentoResponseDTO();
+        assertNotNull(responseDTO);
+        assertEquals(EstabelecimentoResponseDTO.class,responseDTO.getClass());
+
+        Page<EstabelecimentoResponseDTO> responsePage = new PageImpl<>(Collections.singletonList(responseDTO),pageable,1);
+        assertNotNull(responsePage);
+
+        lenient().when(estabelecimentoService.listar(pageable)).thenReturn(responsePage);
+
+        ResponseEntity<Page<EstabelecimentoResponseDTO>> response = estabelecimentoController.listar(pageable);
+
+        assertNotNull(response);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(PageImpl.class, response.getBody().getClass());
+        assertEquals(10,response.getBody().getPageable().getPageSize());
+        assertEquals(0,response.getBody().getPageable().getPageNumber());
+        assertEquals(responsePage, response.getBody());
+        assertThrows(RuntimeException.class,()->{
+            throw new RuntimeException("Erro ao listar o estabelecimento");
+        });
+
+        mockMvc.perform(get(ESTABELECIMENTO_API_URL_PATH)
+                        .param("page","0")
+                        .param("size", "10")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.pageable.pageNumber",is(0)))
+                .andExpect(jsonPath("$.pageable.pageSize", is(10)))
+                .andExpect(jsonPath("$.pageable.sort.empty",is(true)))
+                .andExpect(jsonPath("$.pageable.sort.sorted",is(false)))
+                .andExpect(jsonPath("pageable.sort.unsorted",is(true)))
+                .andExpect(jsonPath("pageable.offset",is(0)))
+                .andExpect(jsonPath("pageable.unpaged",is(false)))
+                .andExpect(jsonPath("pageable.paged",is(true)))
+                .andExpect(jsonPath("$.last",is(true)))
+                .andExpect(jsonPath("$.totalPages",is(1)))
+                .andExpect(jsonPath("$.totalElements",is(1)))
+                .andExpect(jsonPath("$.size",is(10)))
+                .andExpect(jsonPath("$.number",is(0)))
+                .andExpect(jsonPath("$.sort.empty",is(true)))
+                .andExpect(jsonPath("$.sort.sorted",is(false)))
+                .andExpect(jsonPath("$.sort.unsorted",is(true)))
+                .andExpect(jsonPath("$.first",is(true)))
+                .andExpect(jsonPath("$.numberOfElements",is(1)))
+                .andExpect(jsonPath("$.empty",is(false)));
+    }
+
 }
